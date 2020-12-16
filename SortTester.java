23d35bf1d3e1a43154d19ed5d6fb63ac76ae4a22
ElizaKnapp <eknapp20@stuy.edu>
2020-12-16 12:40:35 -0500

    I took out the helper function because for some reason it wasn't working

--- SortTester.java
+++ SortTester.java
@@ -5,7 +5,12 @@ public class SortTester {
   public static void main(String[] args) {
     int[] tester1 = new int[] {5, 1, 122, -5, 16};
     System.out.println(Arrays.toString(tester1));
+    //System.out.println(Arrays.toString(Sorts.swap(tester1, 0, 1))); //swap works
+    //actual loop no
     Sorts.bubbleSort(tester1);
     System.out.println(Arrays.toString(tester1));
+
+
+
   }
 }
--- Sorts.java
+++ Sorts.java
@@ -4,23 +4,13 @@ import java.io.*;
 public class Sorts {
   public static void bubbleSort(int[] data) {
     for (int i = 0; i < data.length - 1; i++) {
-      if (data[i] > data[i + 1]) {
-        data = swap(data, i, i + 1);
+      for (int j = 0; j < data.length - 1 - i; j++) {
+        if (data[j] > data[j + 1]) {
+          int holder = data[j];
+          data[j] = data[j + 1];
+          data[j + 1] = holder;
+        }
       }
     }
-  }
-
-  public static int[] swap(int[] data, int index1, int index2) {
-    //trying to replace index 1 and 2
-    int[] ans = new int[data.length];
-    for (int i = 0; i < index1; i++) {
-      ans[i] = data[i];
-    }
-    ans[index1] = data[index2];
-    ans[index2] = data[index1];
-    for (int i = index2 + 1; i < data.length; i++) {
-      ans[i] = data[i];
-    }
-    return ans;
   }
 }
