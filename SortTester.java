23ffef3cd04b852fd974b89ba73f7596a3f58d62
ElizaKnapp <eknapp20@stuy.edu>
2020-12-16 12:54:12 -0500

    printed lines to check if it was the right sort

--- SortTester.java
+++ SortTester.java
@@ -3,10 +3,9 @@ import java.io.*;
 
 public class SortTester {
   public static void main(String[] args) {
-    int[] tester1 = new int[] {5, 1, 122, -5, 16};
+    int[] tester1 = new int[] {5, 1, 12, -5, 16};
     System.out.println(Arrays.toString(tester1));
-    //System.out.println(Arrays.toString(Sorts.swap(tester1, 0, 1))); //swap works
-    //actual loop no
+
     Sorts.bubbleSort(tester1);
     System.out.println(Arrays.toString(tester1));
 
--- Sorts.java
+++ Sorts.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class Sorts {
   public static void bubbleSort(int[] data) {
     for (int i = 0; i < data.length - 1; i++) {
-      for (int j = 0; j < data.length - 1 - i; j++) {
+      for (int j = 0; j < data.length - 2 - i; j++) {
         if (data[j] > data[j + 1]) {
           int holder = data[j];
           data[j] = data[j + 1];
@@ -12,5 +12,7 @@ public class Sorts {
         }
       }
     }
+
   }
+
 }
