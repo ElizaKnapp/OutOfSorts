b9f546a80610e755d808baf541ec5bcdbd3b1c62
ElizaKnapp <eknapp20@stuy.edu>
2020-12-16 12:08:27 -0500

    helper function created

--- Sorts.java
+++ Sorts.java
@@ -3,8 +3,22 @@ public class Sorts {
   public static void bubbleSort(int[] data) {
     for (int i = 0; i < data.length - 1; i++) {
       if (data[i] > data[i + 1]) {
-        data = swap(data, data[i], data[i + 1]);
+        data = swap(data, i, i + 1);
       }
     }
+  }
+
+  public static int[] swap(int[] data, int index1, int index2) {
+    //trying to replace index 1 and 2
+    int[] ans = new int[data.length];
+    for (int i = 0; i < index1; i++) {
+      ans[i] = data[i];
+    }
+    ans[index1] = data[index2];
+    ans[index2] = data[index1];
+    for (int i = index2 + 1; i < data.length; i++) {
+      ans[i] = data[i];
+    }
+    return ans;
   }
 }
