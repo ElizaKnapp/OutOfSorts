0bd59ce4e96cef0f41d5c0af717b45d7d737e3b9
ElizaKnapp <eknapp20@stuy.edu>
2020-12-17 18:53:52 -0500

    added a count that breaks the loop if everything is already in order

--- Sorts.java
+++ Sorts.java
@@ -3,13 +3,21 @@ import java.io.*;
 
 public class Sorts {
   public static void bubbleSort(int[] data) {
+    int count = 0;
     for (int i = 0; i < data.length - 1; i++) {
+      count = 0;
+      //System.out.println("round " + i);
       for (int j = 0; j < data.length - 1 - i; j++) {
         if (data[j] > data[j + 1]) {
           int holder = data[j];
           data[j] = data[j + 1];
           data[j + 1] = holder;
+          count++;
         }
+        //System.out.println(Arrays.toString(data));
+      }
+      if (count == 0) {
+        i = data.length;
       }
     }
 
