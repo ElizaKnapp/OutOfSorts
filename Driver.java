bb64932e5a3f8c754c0f4b3559c8ef7691ce9802
ElizaKnapp <eknapp20@stuy.edu>
2020-12-22 11:44:09 -0500

    tester files

--- Driver.java
+++ Driver.java
@@ -1,87 +1,67 @@
-import java.util.*;
-import java.io.*;
-
+import java.util.Arrays;
 public class Driver{
-
   public static void main(String[]args){
-    //args 0 must be "bubble" or "test" (unless you add more)
+    if(args.length < 2){
+      System.out.println("usage: java Driver SIZE ALGO [STYLE]");
+      System.out.println("supported ALGO: bubble,selection,insertion,test");
+      System.out.println("supported STYLE: random,equal,sorted,reversed");
+    }else{
 
-    int length = 100; //change this to make the array longer
-    int times = 1000; //change this to make more individual test cases
 
-    if(args[0].equals("bubble")){
-      int error = 0;
-      for (int j = 0; j < times; j++) {
-        Random num = new Random();
-        int seed = num.nextInt()  % 1000;
-        Random tester = new Random(seed);
-        int[] testArr = new int[length];
-          for (int i = 0; i < length; i++) {
-            testArr[i] = tester.nextInt() % 1000;
-          }
-        int[] clone = testArr.clone();
-        Arrays.sort(clone);
-        Sorts.bubbleSort(testArr);
-        if (!Arrays.equals(clone, testArr)) {
-          error++;
+      int[] randData = new int[Integer.parseInt(args[0])];
+
+      //overhead: 1 random array generation.
+      if(args.length < 3 || args[2].equals("random")){
+	      System.out.println("REPLACE THIS LINE WITH ARRAY VALUE GENERATION!");
+      }else if(args[2].equals("equal")){
+	      System.out.println("REPLACE THIS LINE WITH ARRAY VALUE GENERATION!");
+      }else if(args[2].equals("sorted")){
+	      System.out.println("REPLACE THIS LINE WITH ARRAY VALUE GENERATION!");
+      }else if(args[2].equals("reversed")){
+	      System.out.println("REPLACE THIS LINE WITH ARRAY VALUE GENERATION!");
+      }
+
+      if(args[1].equals("bubble")){
+        Sorts.bubbleSort(randData);
+      }
+      else if(args[1].equals("selection")){
+        Sorts.selectionSort(randData);
+      }
+      else if(args[1].equals("insertion")){
+        Sorts.insertionSort(randData);
+      }
+
+      //TEST MODE! This lets you see if your algorithm fails
+      //for a particular type of array, or size of array.
+      //This is not meant to be a timed test...
+      else if(args[1].equals("test")){
+
+        int[] randDataBubble = Arrays.copyOf(randData,randData.length);
+        int[] randDataSelection = Arrays.copyOf(randData,randData.length);
+        int[] randDataInsertion = Arrays.copyOf(randData,randData.length);
+        Arrays.sort(randData);
+        Sorts.bubbleSort(randDataBubble);
+        Sorts.selectionSort(randDataSelection);
+        Sorts.insertionSort(randDataInsertion);
+
+        if( Arrays.equals(randData,randDataBubble)){
+          System.out.println("Bubble Correct!");
+        }else{
+         System.out.println("Bubble Not Correct!!!!!!!!!11oneeleven");
+        }
+
+        if( Arrays.equals(randData,randDataSelection)){
+          System.out.println("Selection Correct!");
+        }else{
+         System.out.println("Selection Not Correct!!!!!!!!!11oneeleven");
+        }
+
+        if( Arrays.equals(randData,randDataInsertion)){
+          System.out.println("Insertion Correct!");
+        }else{
+         System.out.println("Insertion Not Correct!!!!!!!!!11oneeleven");
         }
       }
-      if (error == 0) {
-        System.out.println("everything works");
-      }
-      else {
-        System.out.println("there is a problem with random sort no duplicates");
-      }
     }
-    if(args[0].equals("selection")){
-      int error = 0;
-      for (int j = 0; j < times; j++) {
-        Random num = new Random();
-        int seed = num.nextInt()  % 1000;
-        Random tester = new Random(seed);
-        int[] testArr = new int[length];
-          for (int i = 0; i < length; i++) {
-            testArr[i] = tester.nextInt() % 1000;
-          }
-        int[] clone = testArr.clone();
-        Arrays.sort(clone);
-        Sorts.selectionSort(testArr);
-        if (!Arrays.equals(clone, testArr)) {
-          error++;
-        }
-      }
-      if (error == 0) {
-        System.out.println("everything works");
-      }
-      else {
-        System.out.println("there is a problem with random sort no duplicates");
-      }
-    }
-    if(args[0].equals("insertion")){
-      int error = 0;
-      for (int j = 0; j < times; j++) {
-        Random num = new Random();
-        int seed = num.nextInt()  % 1000;
-        Random tester = new Random(seed);
-        int[] testArr = new int[length];
-          for (int i = 0; i < length; i++) {
-            testArr[i] = tester.nextInt() % 1000;
-          }
-        int[] clone = testArr.clone();
-        Arrays.sort(clone);
-        Sorts.insertionSort(testArr);
-        if (!Arrays.equals(clone, testArr)) {
-          error++;
-        }
-      }
-      if (error == 0) {
-        System.out.println("everything works");
-      }
-      else {
-        System.out.println("there is a problem with random sort no duplicates");
-      }
-    }
-
-
   }
 }
