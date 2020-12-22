1053c069cc16fbd9b7c6f62f19a95b91050ef30a
ElizaKnapp <eknapp20@stuy.edu>
2020-12-21 20:17:14 -0500

    switched from driver to my own tester because I looked at tmrws hw and realized that the driver is super specific

--- SortTester.java
+++ SortTester.java
@@ -1,46 +1,87 @@
 import java.util.*;
 import java.io.*;
 
-public class SortTester {
-  public static void main(String[] args) {
+public class SortTester{
+
+  public static void main(String[]args){
+    //args 0 must be "bubble" or "test" (unless you add more)
 
     int length = 100; //change this to make the array longer
     int times = 1000; //change this to make more individual test cases
 
-    int error = 0;
-    for (int j = 0; j < times; j++) {
-      Random num = new Random();
-      int seed = num.nextInt()  % 1000;
-      Random tester = new Random(seed);
-      int[] testArr = new int[length];
-        for (int i = 0; i < length; i++) {
-          testArr[i] = tester.nextInt() % 1000;
+    if(args[0].equals("bubble")){
+      int error = 0;
+      for (int j = 0; j < times; j++) {
+        Random num = new Random();
+        int seed = num.nextInt()  % 1000;
+        Random tester = new Random(seed);
+        int[] testArr = new int[length];
+          for (int i = 0; i < length; i++) {
+            testArr[i] = tester.nextInt() % 1000;
+          }
+        int[] clone = testArr.clone();
+        Arrays.sort(clone);
+        Sorts.bubbleSort(testArr);
+        if (!Arrays.equals(clone, testArr)) {
+          error++;
         }
-      int[] clone = testArr.clone();
-      Arrays.sort(clone);
-      //Substitute here for different sorts
-      Sorts.insertionSort(testArr);
-      if (!Arrays.equals(clone, testArr)) {
-        error++;
+      }
+      if (error == 0) {
+        System.out.println("everything works");
+      }
+      else {
+        System.out.println("there is a problem with random sort no duplicates");
       }
     }
-    if (error == 0) {
-      System.out.println("everything works");
+    if(args[0].equals("selection")){
+      int error = 0;
+      for (int j = 0; j < times; j++) {
+        Random num = new Random();
+        int seed = num.nextInt()  % 1000;
+        Random tester = new Random(seed);
+        int[] testArr = new int[length];
+          for (int i = 0; i < length; i++) {
+            testArr[i] = tester.nextInt() % 1000;
+          }
+        int[] clone = testArr.clone();
+        Arrays.sort(clone);
+        Sorts.selectionSort(testArr);
+        if (!Arrays.equals(clone, testArr)) {
+          error++;
+        }
+      }
+      if (error == 0) {
+        System.out.println("everything works");
+      }
+      else {
+        System.out.println("there is a problem with random sort no duplicates");
+      }
     }
-    else {
-      System.out.println("there is a problem with random sort no duplicates");
+    if(args[0].equals("insertion")){
+      int error = 0;
+      for (int j = 0; j < times; j++) {
+        Random num = new Random();
+        int seed = num.nextInt()  % 1000;
+        Random tester = new Random(seed);
+        int[] testArr = new int[length];
+          for (int i = 0; i < length; i++) {
+            testArr[i] = tester.nextInt() % 1000;
+          }
+        int[] clone = testArr.clone();
+        Arrays.sort(clone);
+        Sorts.insertionSort(testArr);
+        if (!Arrays.equals(clone, testArr)) {
+          error++;
+        }
+      }
+      if (error == 0) {
+        System.out.println("everything works");
+      }
+      else {
+        System.out.println("there is a problem with random sort no duplicates");
+      }
     }
 
-    int[] test = new int[] {6, 5, 4, 3, 2, 1};
-    Sorts.insertionSort(test);
-    System.out.println(Arrays.toString(test));
-
-    //here- add test cases that are already sorted and reverse sorted
-    //also random with many duplicates
-
-    //stable sort maintains original order, like bubble sort, but not Selection Sort
-    //insertion sort
-    //use time java
 
   }
 }
